e87073e5b5c7ec4f9418bbcd956015b75a7be02f
sanjaymehra <mehra271198@gmail.com>
2022-08-02 15:02:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# whatsapp_share2_android
+ this whatsapp share package works only android in flutter
---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Applications/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/mac/Downloads/whatsapp_share2-main/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/mac/Downloads/whatsapp_share2-main/example/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/mac/Downloads/whatsapp_share2-main/example/.dart_tool/package_config.json"
